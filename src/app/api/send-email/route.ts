86a6784d3f9b9fbf2a04e82fef82902bf716aa17
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-08-01 22:43:56 -0300

    feat: add whatsapp

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -49,19 +49,21 @@ ${message}`,
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
-    body { font-family: Arial, sans-serif; color: #333; }
-    .container { max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 8px; background-color: #f9f9f9; }
-    h1 { font-size: 24px; color: #333; }
-    p { font-size: 16px; line-height: 1.5; margin: 10px 0; }
-    .info { border-bottom: 1px solid #ddd; padding-bottom: 10px; margin-bottom: 10px; }
+    body { font-family: Arial, sans-serif; color: #333; margin: 0; padding: 0; }
+    .container { max-width: 600px; margin: 0 auto; padding: 20px; border: 1px solid #ddd; border-radius: 10px; background-color: #ffffff; box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); }
+    .header { text-align: center; border-bottom: 3px solid #007bff; padding-bottom: 10px; margin-bottom: 20px; }
+    .header img { width: 150px; height: auto; }
+    .title { font-size: 24px; color: #007bff; margin-bottom: 10px; }
+    .info { margin-bottom: 20px; }
     .info p { margin: 5px 0; }
-    .info img { vertical-align: middle; }
+    .info strong { color: #333; }
     .footer { font-size: 14px; color: #666; text-align: center; margin-top: 20px; }
   </style>
 </head>
 <body>
   <div class="container">
-    <h1>Nova Mensagem de Contato</h1>
+
+    <div class="title">Nova Mensagem de Contato</div>
     <div class="info">
       <p><strong>Nome:</strong> ${name}</p>
       <p><strong>E-mail:</strong> ${contactByEmail ? email : 'Não informado'}</p>
@@ -69,7 +71,10 @@ ${message}`,
       <p><strong>Método de Contato Preferido:</strong> ${contactByEmail ? 'Email' : ''} ${contactByPhone ? 'Telefone' : ''}</p>
     </div>
     <p><strong>Mensagem:</strong><br>${message}</p>
-    <p><img src="https://bm-monobloco.vercel.app/logo.png" alt="Logo da Bm Monobloco" style="width: 200px; height: auto; display: block; margin: 20px auto;" /></p>
+    <div class="footer">
+      <p>Equipe Bm Monobloco</p>
+      <p><img src="https://bm-monobloco.vercel.app/logo.png" alt="Logo da Bm Monobloco" style="width: 200px; height: auto; display: block; margin: 20px auto;" /></p>
+    </div>
   </div>
 </body>
 </html>`,
@@ -79,7 +84,7 @@ ${message}`,
       from: 'kelven.fontes@blindjobs.com.br',
       to: email,
       subject: 'Obrigado por entrar em contato!',
-      text: `Olá ${name},\n\nAgradecemos por entrar em contato com a Bm Monobloco!\n\nRecebemos sua mensagem e nossa equipe estará avaliando e retornará a você o mais breve possível.\n\nAgradecemos pelo seu interesse e confiança em nossos serviços.\n\nAtenciosamente,\nBm Monobloco\n\nMensagem:\n${message}`,
+      text: `Olá ${name},\n\nAgradecemos por entrar em contato com a Bm Monobloco!\n\nRecebemos sua mensagem e nossa equipe estará avaliando e retornará a você o mais breve possível.\n\nMensagem:\n${message}\n\nAgradecemos pelo seu interesse e confiança em nossos serviços.\n\nAtenciosamente,\nBm Monobloco`,
       html: `<p>Olá ${name},</p>
              <p>Agradecemos por entrar em contato com a <strong>Bm Monobloco</strong>!</p>
              <p>Recebemos sua mensagem e nossa equipe estará avaliando e retornará a você o mais breve possível.</p>
