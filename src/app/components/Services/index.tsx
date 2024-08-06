23b3f3db4e1ba86260a270606ac3afa6bea0384b
Kelven Fontes <kelvenbentofontes@gmail.com>
2024-08-06 20:17:38 -0300

    feat: add manutenção geral

--- src/app/components/Services/index.tsx
+++ src/app/components/Services/index.tsx
@@ -9,64 +9,64 @@ const services = [
 
 const Services = () => {
   return (
-    <section className = "bg-gray-50 p-12 rounded-lg shadow-lg mb-4 mt-4 w-[93%]" id='services'>
-       <h2 className="text-3xl font-bold text-gray-900 mb-6 text-center">Nossos Serviços</h2>
-       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-         {/* Serviço 1 */}
-         <motion.div
-           className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
-           initial={{ opacity: 0 }}
-           animate={{ opacity: 1 }}
-           transition={{ duration: 0.6 }}
-         >
-           <FaTools className="text-blue-500 text-5xl mb-4 mx-auto" />
-           <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Alinhamento de Chassi</h3>
-           <p className="text-gray-700 text-center">
-             Alinhamento técnico de chassi para garantir a estabilidade do veículo e melhorar a dirigibilidade.
-           </p>
-         </motion.div>
-         {/* Serviço 2 */}
-         <motion.div
-           className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
-           initial={{ opacity: 0 }}
-           animate={{ opacity: 1 }}
-           transition={{ duration: 0.6 }}
-         >
-           <FaCar className="text-green-500 text-5xl mb-4 mx-auto" />
-           <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Alinhamento de Longarinas</h3>
-           <p className="text-gray-700 text-center">
-             Serviço especializado em alinhamento de longarinas para corrigir a geometria e melhorar o desempenho do veículo.
-           </p>
-         </motion.div>
-         {/* Serviço 3 */}
-         <motion.div
-           className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
-           initial={{ opacity: 0 }}
-           animate={{ opacity: 1 }}
-           transition={{ duration: 0.6 }}
-         >
-           <FaWrench className="text-red-500 text-5xl mb-4 mx-auto" />
-           <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Manutenção de Freios</h3>
-           <p className="text-gray-700 text-center">
-             Manutenção completa dos sistemas de freios para garantir a segurança e eficiência do seu veículo.
-           </p>
-         </motion.div>
-         {/* Serviço 4 */}
-         <motion.div
-           className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
-           initial={{ opacity: 0 }}
-           animate={{ opacity: 1 }}
-           transition={{ duration: 0.6 }}
-         >
-           <FaCogs className="text-yellow-500 text-5xl mb-4 mx-auto" />
-           <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Solda Mig</h3>
-           <p className="text-gray-700 text-center">
-             Solda Mig de alta qualidade para reparos e manutenção de peças metálicas.
-           </p>
-         </motion.div>
-         {/* Adicione mais blocos de serviço conforme necessário */}
-       </div>
-     </section>
+    <section className="bg-gray-50 p-12 rounded-lg shadow-lg mb-4 mt-4 w-[93%]" id='services'>
+      <h2 className="text-3xl font-bold text-gray-900 mb-6 text-center">Nossos Serviços</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        {/* Serviço 1 */}
+        <motion.div
+          className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6 }}
+        >
+          <FaTools className="text-blue-500 text-5xl mb-4 mx-auto" />
+          <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Alinhamento de Chassi</h3>
+          <p className="text-gray-700 text-center">
+            Alinhamento técnico de chassi para garantir a estabilidade do veículo e melhorar a dirigibilidade.
+          </p>
+        </motion.div>
+        {/* Serviço 2 */}
+        <motion.div
+          className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6 }}
+        >
+          <FaCar className="text-green-500 text-5xl mb-4 mx-auto" />
+          <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Alinhamento de Longarinas</h3>
+          <p className="text-gray-700 text-center">
+            Serviço especializado em alinhamento de longarinas para corrigir a geometria e melhorar o desempenho do veículo.
+          </p>
+        </motion.div>
+        {/* Serviço 3 */}
+        <motion.div
+          className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6 }}
+        >
+          <FaWrench className="text-red-500 text-5xl mb-4 mx-auto" />
+          <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Manutenção de Suspensão Geral</h3>
+          <p className="text-gray-700 text-center">
+            Garantimos a segurança e eficiência do seu veículo com nossa manutenção completa da suspensão.
+          </p>
+        </motion.div>
+        {/* Serviço 4 */}
+        <motion.div
+          className="bg-white p-6 rounded-lg shadow-md transition-transform transform hover:scale-105"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.6 }}
+        >
+          <FaCogs className="text-yellow-500 text-5xl mb-4 mx-auto" />
+          <h3 className="text-xl font-semibold text-gray-900 mb-2 text-center">Solda Mig</h3>
+          <p className="text-gray-700 text-center">
+            Solda Mig de alta qualidade para reparos e manutenção de peças metálicas.
+          </p>
+        </motion.div>
+        {/* Adicione mais blocos de serviço conforme necessário */}
+      </div>
+    </section>
     // <section id="services" className="py-12 bg-white">
     //   <div className="container mx-auto px-4">
     //     <h2 className="text-3xl font-bold text-center text-gray-900">Nossos Serviços</h2>
